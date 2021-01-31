b31b05b59c53cc492598ed1ff25a75f70b62476e
Lucky <lucky@mlmparts.com>
2021-01-31 15:41:20 +0800

    [ADD]ApiGetProductById

---
+++ ApiGetProductById/registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Copyright 2020 webtek. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Lucky_ApiGetProductById',
+    __DIR__
+);
